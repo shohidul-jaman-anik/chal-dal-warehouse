c0a24a6dd38d7e2c4a2669cdb902a85684a0688a
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-05-07 23:05:11 +0600

    Environment Varibale Setup Complete

--- src/Hooks/firebase.init.js
+++ src/Hooks/firebase.init.js
@@ -9,13 +9,21 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//         apiKey: "AIzaSyC1omMUDtn-HWYvNJWf8QStILlbOKgaAx0",
+//         authDomain: "chaldal-warehouse.firebaseapp.com",
+//         projectId: "chaldal-warehouse",
+//         storageBucket: "chaldal-warehouse.appspot.com",
+//         messagingSenderId: "1070662638738",
+//         appId: "1:1070662638738:web:62f4d2b1ca219ddd155a4b"
+// };
 const firebaseConfig = {
-        apiKey: "AIzaSyC1omMUDtn-HWYvNJWf8QStILlbOKgaAx0",
-        authDomain: "chaldal-warehouse.firebaseapp.com",
-        projectId: "chaldal-warehouse",
-        storageBucket: "chaldal-warehouse.appspot.com",
-        messagingSenderId: "1070662638738",
-        appId: "1:1070662638738:web:62f4d2b1ca219ddd155a4b"
+        apiKey: process.env.REACT_APP_apiKey,
+        authDomain: process.env.REACT_APP_authDomain,
+        projectId: process.env.REACT_APP_projectId,
+        storageBucket: process.env.REACT_APP_storageBucket,
+        messagingSenderId: process.env.REACT_APP_messagingSenderId,
+        appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
